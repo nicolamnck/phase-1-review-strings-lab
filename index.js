bb865346b5751f6c2c7b83d799bfd2bdbc44f21c
¨NicolaMarie¨ <nicolamarie.nm@gmail.com>
2024-07-04 19:58:13 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser}`;
+
+currentUser.slice(0, 1);
+
+shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
